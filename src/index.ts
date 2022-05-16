87b23fefc120a6718e69c36d838069318418e9db
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-05-15 23:29:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let nota, suma, promedio, contador: number;
+suma = 0;
+
+for (contador = 1; contador <= 10; contador++) {
+  nota = Number(prompt("ingrese una nota " + contador + ":"));
+  suma = suma + nota;
+}
+promedio = suma / 10;
+console.log("El promedio es de: ", promedio);
